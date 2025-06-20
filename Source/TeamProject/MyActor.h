d6fc28865204db2e7fba7b8c08b017afa9b2622d
Parkjunbeom99 <142134462+Parkjunbeom99@users.noreply.github.com>
2025-06-20 19:15:39 +0900

    로컬 브랜치 생성 및 커밋
    main 로컬 브랜치를 세이브 포인트로 쓰기 위해 작업 내용은 Feature 로컬 브랜치로 따로 파서 진행하는 방법을 배웠다 .
    새 로컬 브랜치에서 작업 후 main 로컬 브랜치로 머지하는 연습을 해봤다

--- Source/TeamProject/MyActor.h
+++ Source/TeamProject/MyActor.h
@@ -15,12 +15,6 @@ public:
 	// Sets default values for this actor's properties
 	AMyActor();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
 };
